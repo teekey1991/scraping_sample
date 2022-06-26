636ad71a47edafec934c975a14fecd4d723fde7f
teekey1991 <matsunaetaiki@gmail.com>
2022-06-26 11:45:26 +0900

    resipe_check完了

--- recipe_check.py
+++ recipe_check.py
@@ -1,28 +1,42 @@
-import bs4
+# urljoinを利用する場合はインポート
+# from urllib.parse import urljoin
 import requests
+import bs4
+
+
+def get_recipes_with_cookpad(food):
+    '''
+    cookpadからキーワード検索したレシピの、タイトルとURLの配列を返す
+    '''
+
+    base_url = "https://cookpad.com"
+    search_url = f"{base_url}/search/{food}"
+
+    res = requests.get(search_url)
+
+    soup = bs4.BeautifulSoup(res.content, "html.parser")
+    recipe_previews = soup.find_all(class_="recipe-preview")
+
+    recipes = []
+    for recipe_preview in recipe_previews:
+        a_tag = recipe_preview.find(class_="recipe-title")
+        recipes.append(
+            {"title": a_tag.text, "url": f"{base_url}{a_tag.attrs['href']}"}
+            # urljoinで「/」の有無を気にせず連結できる(URLの場合)
+            # {"title": a_tag.text, "url": urljoin(res.url, a_tag.attrs["href"])}
+        )
+
+    return recipes
 
 
 def main():
-    food = "とまと"
-    recipes = find_recipe(food)
-    print(recipes)
+    food = "トマト"
 
-def find_recipe(food):
-    base_url = "https://cookpad.com"
+    recipes = get_recipes_with_cookpad(food)
 
-    res = requests.get(f"{base_url}/search/{food}")
-
-    soup = bs4.BeautifulSoup(res.text, "html.parser")
-
-    # classがrecipe-previewの要素を取得
-    recipe_previews = soup.find_all(class_="recipe-preview")
-
-    # recipe-previewsからタイトルとURLを取得
-    recipes = []
-    for recipe_preview in recipe_previews:
-        a_tag = recipe_preview.find("a", class_="recipe-title")
-        recipes.append({"title": a_tag.text, "url": f"{base_url}{a_tag.attrs['href']}"})
-    return recipes
+    for recipe in recipes:
+        food_info = f'レシピ名 {recipe["title"]}, URL {recipe["url"]}'
+        print(food_info)
 
 
 if __name__ == "__main__":
